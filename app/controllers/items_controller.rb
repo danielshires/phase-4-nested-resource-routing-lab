a434a6351c4166451125fae1731efb653126e714
danielshires <hello@danielshires.com>
2022-11-06 19:25:12 +1100

    First commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,26 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
 
   def index
     items = Item.all
     render json: items, include: :user
   end
+
+  def show
+    items = Item.find_by(id: params[:id])
+    render json: items, include: :user
+  end 
+
+  # def create
+
+  # end
+
+  private
+
+  def render_not_found_response
+    render json: {error: "Record not found"}, status: :not_found
+  end
+
 
 end
